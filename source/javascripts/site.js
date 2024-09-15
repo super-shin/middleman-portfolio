f7a2bbe9a7d953a452d9a79760e40842c1ce572f
Shingie Taira <tairashingie@gmail.com>
2024-09-15 19:33:48 +0900

    fixed the left side of the about section

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
 // This is where it all goes :)
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollDown = document.getElementById('scroll-down');
+    if (scrollDown) {
+        scrollDown.addEventListener('click', function() {
+            var aboutSection = document.getElementById('about');
+            if (aboutSection) {
+                aboutSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
+});
